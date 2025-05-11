a20787a0d90eb481b07245d96d71fe2d076652eb
gorapaduckoo <jioung93@naver.com>
2025-05-12 01:17:14 +0900

    [BOJ 2073] 수도배관공사

--- 2025-05/Main_2073_수도배관공사.java
+++ 2025-05/Main_2073_수도배관공사.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_2073_수도공사 {
+public class Main_2073_수도배관공사 {
 	static int D, P;
 	static int[] length, capacity;
 	static int[] dp;
